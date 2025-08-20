d0382cb044083351b18cf45371d840b4365807b6
officialshivangik11@gmail.com <officialshivangik11@gmail.com>
2025-08-21 02:09:39 +0530

    styling hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+  <div className="relative w-full h-screen overflow-hidden">
+  <video autoPlay muted loop className="absolute w-auto min-w-full min-h-full max-w-none z-0">
+    <source src="/videos/shopit_header.mp4" type="video/mp4" />
+ 
+  </video>
+
+  <div className="absolute inset-0   flex items-center justify-left z-10">
+
+    <div className="text-white text-7xl font-[Parisienne] font-bold bg-black bg-opacity-50 p-4 rounded">
+     Luxury,
+    </div>
+    <div className="text-white text-7xl font-[Parisienne] font-bold bg-black bg-opacity-50 p-4 rounded">
+    Redefined for You.
+    </div>
+  </div>
+</div>
+
   )
 }
 
